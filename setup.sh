be8178a93c752adfea3f9ae776a487e26895ff5d
ltnprogithub <152204404+ltnprogithub@users.noreply.github.com>
2024-06-01 13:46:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd && wget https://github.com/jzinferno/termux-ndk/releases/download/v1/android-ndk-r24-aarch64.zip && unzip android-ndk-r24-aarch64.zip && rm -rf android-ndk-r24-aarch64.zip && mkdir android-sdk/ndk && mv android-ndk-r24 24.0.8215888 && mv 24.0.8215888 android-sdk/ndk/ && ln -s $HOME/android-sdk/ndk/24.0.8215888/toolchains/llvm/prebuilt/linux-aarch64 $HOME/android-sdk/ndk/24.0.8215888/toolchains/llvm/prebuilt/linux-x86_64 && ln -s $HOME/android-sdk/ndk/24.0.8215888/prebuilt/linux-aarch64 $HOME/android-sdk/ndk/24.0.8215888/prebuilt/linux-x86_64 && echo 'Installation Finished. Ndk has been installed successfully!'
+cd
 install_dir=$HOME
 sdk_dir=$install_dir/android-sdk
 ndk_dir=$sdk_dir/ndk/24.0.8215888
@@ -8,7 +9,7 @@ echo "[NOTICE] This cmake is for aarch64 only !"
 echo "[DEBUG] Patching existing cmake config from ${ndk_dir}/build/cmake/android-legacy.toolchain.cmake"
 cd $install_dir
 
-# patching cmake config
+
 sed -i 's/if(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/if(CMAKE_HOST_SYSTEM_NAME STREQUAL Android)\nset(ANDROID_HOST_TAG linux-aarch64)\nelseif(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/g' $ndk_dir/build/cmake/android-legacy.toolchain.cmake
 sed -i 's/if(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/if(CMAKE_HOST_SYSTEM_NAME STREQUAL Android)\nset(ANDROID_HOST_TAG linux-aarch64)\nelseif(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)/g' $ndk_dir/build/cmake/android.toolchain.cmake
 
